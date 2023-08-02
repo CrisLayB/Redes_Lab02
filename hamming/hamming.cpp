06a380e78c8ad44bd4c3d11e4693df4fb7387f50
CRLB-Sketch <cristianlaynezbachez@gmail.com>
2023-08-01 23:08:26 -0400

    C++ hamming emisor.

--- hamming/hamming.cpp
+++ hamming/hamming.cpp
@@ -1,9 +1,4 @@
-// Referencia Algoritmos
-// https://html.rincondelvago.com/codigo-de-hamming.html
-// https://chovis2.tripod.com/CODIGO_HAMMING.HTM
-// https://www.youtube.com/watch?v=ay9LRC4IdpM
-// https://www.youtube.com/watch?v=3Td-aqtD14M
-// https://www.geeksforgeeks.org/hamming-code-in-computer-network/
+// C++ Emisor
 
 // Comando para ejecutar:
 // g++ hamming/hamming.cpp -o hamming/hamming
@@ -12,27 +7,32 @@
 #include <iostream>
 #include <bits/stdc++.h>
 
-void hammingCode(std::string bits);
+struct Code {
+    int amount;
+    int* arrayCode;
+};
+
+Code hammingCode(std::string bits);
 bool verifyPow(int numVerify, int amountNums);
+void printCode(int* arr, int totalLong);
+void printCode(int& arr);
+
 
 // 2^m >= m + r + 1
-void hammingCode(std::string bits)
+Code hammingCode(std::string bits)
 {
     int m = bits.length();
     int r = 0;
 
+    std::cout << "Input: " << bits;
+
     // 2^r >= m + r + 1
     while(pow(2, r) < (m + r + 1)) r++;
 
-    std::cout << "m: " << m << std::endl;
-    std::cout << "r: " << r << std::endl;
-
     int totalLong = m + r;
-
-    std::cout << "Total Long: " << totalLong << std::endl;
-
     int nums[totalLong];
 
+    // Define bits in the arrayCode
     int counter = 0;
     for(int i = totalLong - 1; i > -1; i--)
     {
@@ -48,27 +48,39 @@ void hammingCode(std::string bits)
         }
     }
 
-    for(int t = 0; t < r; t++)
+    // Calculate Parity
+    for(int i = 0; i < r; i++)
     {
-        int exp = pow(2, t);
-        std::cout << "=> " << t << " exp: " << exp << std::endl;
-        for(int i = 1; i < totalLong; i++)
+        int exp = pow(2, i);
+        int controller = 0, countOnes = 0;
+        bool isOne = false;
+
+        for(int j = 0; j < totalLong; j++) // check amount ones
         {
-            if (((i >> t) & 1) == 1)
-            {
-                if (exp != i) nums[exp] = nums[exp] ^ nums[i];
+            controller++;
+            if(controller >= exp){ // Make Swap
+                isOne = !isOne;
+                controller = 0;
             }
-            std::cout << i << std::endl;
-            // std::cout << i << " : " << nums[i] << std::endl;
+
+            if(nums[j] == 1 && isOne) countOnes++;
         }
+
+        nums[exp - 1] = (countOnes % 2 == 0) ? 0 : 1;
     }
 
-    for(int i = 0; i < totalLong; i++)
-    {
-        std::cout << i << " : " << nums[i] << std::endl;
+    // Flip values
+    for (int i = 0; i < totalLong / 2; ++i) {
+        int temp = nums[i];
+        nums[i] = nums[totalLong - 1 - i];
+        nums[totalLong - 1 - i] = temp;
     }
-    
-    return;
+
+    std::cout << "\nOutput: ";
+    printCode(nums, totalLong);
+    Code results;
+    results.amount = totalLong;
+    return results;
 }
 
 bool verifyPow(int numVerify, int amountNums)
@@ -83,12 +95,21 @@ bool verifyPow(int numVerify, int amountNums)
     return false;
 }
 
+void printCode(int* arr, int totalLong)
+{
+    for(int i = 0; i < totalLong; i++)
+    {
+        std::cout << arr[i];
+    }
+
+    std::cout << std::endl;
+}
+
 int main()
 {
-    // verifyPow(9, 4);
-    // verifyPow(8, 4);
-    // 1001 = 4
-    hammingCode("1011001");
+    Code results = hammingCode("1011001");
+    // std::cout << "RESULTADOS: " << std::endl;
+    // printCode(results.arrayCode, results.amount);
     // hammingCode("0111011");
     // hammingCode("1001");
 
