4cc280981111393756c3dc7f2fb62c94430dbacb
Cristian Laynez <54615310+CrisLayB@users.noreply.github.com>
2023-08-04 23:10:51 -0600

    For Deleviry

--- hamming/Hamming.java
+++ hamming/Hamming.java
@@ -13,8 +13,6 @@
 
 package hamming;
 
-import java.util.ArrayList;
-
 class Colors {
     public static final String RESET = "\033[0m";  // Text Reset
     public static final String RED = "\033[0;31m";     // RED
