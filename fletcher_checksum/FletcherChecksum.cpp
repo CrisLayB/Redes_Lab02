a53e18f713ba08a9ed32170bd2c08968d576c572
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2023-08-11 18:19:51 -0600

    Update FletcherChecksum.cpp

--- fletcher_checksum/FletcherChecksum.cpp
+++ fletcher_checksum/FletcherChecksum.cpp
@@ -9,88 +9,120 @@
  *      Cristian Laynez,
  *      Mario de León
  * ]
-************************************************************/
+ *************************************************************/
 
 #include <iostream>
-#include <fstream>
-#include <vector>
-#include <bitset>
-#include <algorithm>
+#include <string>
+#include <cstring>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
-std::string obtenerChecksum(std::string mensaje) {
+// Función para verificar el checksum del mensaje recibido
+bool verificar_checksum(const std::string &mensaje_emisor) {
     // Obtener el checksum del mensaje
-    std::string checksumEmisor = mensaje.substr(mensaje.length() - 8);
-    std::cout << "Checksum recibido: " << checksumEmisor << std::endl;
+    std::string checksum_emisor = mensaje_emisor.substr(mensaje_emisor.length() - 8);
 
     // Obtener el mensaje original sin el checksum y sin espacios en blanco entre bloques de 8
-    std::string mensajeOriginal = mensaje.substr(0, mensaje.length() - 8);
-    mensajeOriginal.erase(std::remove_if(mensajeOriginal.begin(), mensajeOriginal.end(), ::isspace), mensajeOriginal.end());
-    std::cout << "Mensaje original: " << mensajeOriginal << std::endl;
+    std::string mensaje_original = mensaje_emisor.substr(0, mensaje_emisor.length() - 8);
+    mensaje_original.erase(std::remove_if(mensaje_original.begin(), mensaje_original.end(), ::isspace), mensaje_original.end());
 
     // Dividir la cadena en grupos de 8 bits
     std::vector<std::string> binary_blocks;
-    for (int i = 0; i < mensajeOriginal.length(); i += 8) {
-        std::string bloque = mensajeOriginal.substr(i, 8);
-        binary_blocks.push_back(bloque);
+    for (size_t i = 0; i < mensaje_original.length(); i += 8) {
+        binary_blocks.push_back(mensaje_original.substr(i, 8));
     }
 
     // Calcular el checksum del mensaje recibido
     std::string new_string = "00000000";
-
-    for (const std::string& binary_block : binary_blocks) {
-        int integer = std::stoi(binary_block, nullptr, 2); 
-        int total_int_value = std::stoi(new_string, nullptr, 2); 
+    for (const std::string &binary_block : binary_blocks) {
+        int integer = std::stoi(binary_block, nullptr, 2);
+        int total_int_value = std::stoi(new_string, nullptr, 2);
         int bit_sum = integer + total_int_value;
-        
+
         if (bit_sum > 255) {
             bit_sum = (bit_sum & 255) + 1;
         }
 
-        // Realiza la suma directamente en binario con longitud fija de 8 bits
+        // Realizar la suma directamente en binario con longitud fija de 8 bits
         std::string binary_total_sum = std::bitset<8>(bit_sum).to_string();
         new_string = binary_total_sum;
     }
 
-    // Realiza el complemento a uno de la suma final usando XOR (^)
-    for (char& bit : new_string) {
-        bit = bit == '0' ? '1' : '0';
+    // Realizar el complemento a uno de la suma final usando XOR (^)
+    for (char &bit : new_string) {
+        bit = (bit == '0') ? '1' : '0';
     }
 
-    return new_string;
+    // Comparar los checksums
+    return checksum_emisor == new_string;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cout << "No ingresaste el nombre del archivo de texto esperado" << std::endl;
-        std::cout << "\nEjemplo de input esperado:" << std::endl;
-        std::cout << "./receptor mensaje_emisor.txt" << std::endl;
-        return 0;
+// Función para obtener el mensaje original sin el checksum
+std::string obtener_mensaje_original(const std::string &mensaje_emisor) {
+    return mensaje_emisor.substr(0, mensaje_emisor.length() - 8);
+}
+
+// Función para decodificar el mensaje original y presentar la información
+void decodificar_mensaje(const std::string &mensaje_original) {
+    // *************** Capa presentacion ***************
+    std::string mensaje_decodificado = "";
+    // Dividir el mensaje original en grupos de 8 bits y decodificar
+    for (size_t i = 0; i < mensaje_original.length(); i += 8) {
+        std::string bloque = mensaje_original.substr(i, 8);
+        int decimal_value = std::stoi(bloque, nullptr, 2);
+        char caracter = static_cast<char>(decimal_value);
+        mensaje_decodificado += caracter;
     }
 
-    std::string rutaArchivo = argv[1];
-    std::ifstream archivo(rutaArchivo);
+    // *************** Capa aplicacion ***************
+    std::cout << "Mensaje Decodificado: " << mensaje_decodificado << std::endl;
+}
 
-    if (!archivo) {
-        std::cout << "Error al abrir el archivo: " << rutaArchivo << std::endl;
-        return 0;
-    }
-
-    // Leer el mensaje final del archivo
-    std::string mensajeFinal;
-    std::getline(archivo, mensajeFinal);
-
+int main() {
+    // *************** Capa transmision ***************
     std::cout << "\n***RECEPTOR***" << std::endl;
+    // Configuración del socket del servidor
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in server_address{};
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(12345);
 
-    std::string checksumRecibido = obtenerChecksum(mensajeFinal);
-    std::string checksumCalculado = checksumRecibido;
+    bind(server_socket, reinterpret_cast<struct sockaddr *>(&server_address), sizeof(server_address));
+    listen(server_socket, 1);
 
-    if (checksumRecibido == mensajeFinal.substr(mensajeFinal.length() - 8)) {
-        std::cout << "El mensaje es valido." << std::endl;
-        std::cout << "Mensaje Original: " << mensajeFinal.substr(0, mensajeFinal.length() - 8) << std::endl;
-    } else {
-        std::cout << "El mensaje ha sido alterado durante la transmisión." << std::endl;
+    std::cout << "Esperando conexiones del emisor..." << std::endl;
+
+    while (true) {
+        sockaddr_in client_address{};
+        socklen_t client_address_length = sizeof(client_address);
+        int client_socket = accept(server_socket, reinterpret_cast<struct sockaddr *>(&client_address), &client_address_length);
+
+        char buffer[1024];
+        memset(buffer, 0, sizeof(buffer));
+        recv(client_socket, buffer, sizeof(buffer), 0);
+        std::string mensaje_emisor = buffer;
+        std::cout << "Mensaje recibido del emisor: " << mensaje_emisor << std::endl;
+
+        // *************** Capa enlace ***************
+        // Verificar el checksum
+        if (verificar_checksum(mensaje_emisor)) {
+            std::cout << "El mensaje es válido." << std::endl;
+            std::string mensaje_original = obtener_mensaje_original(mensaje_emisor);
+
+            // Llamar a la función de decodificación
+            decodificar_mensaje(mensaje_original);
+        } else {
+            std::cout << "El mensaje ha sido alterado durante la transmisión." << std::endl;
+        }
+
+        // Cerrar el socket del cliente
+        close(client_socket);
     }
 
-    archivo.close();
+    // Cerrar el socket del servidor (esto solo se ejecutará si el bucle se detiene)
+    close(server_socket);
+
     return 0;
 }
