5582f621d801692cdbc31dde22325b9baf8f7faf
Mario de León <46909802+DavidDLM@users.noreply.github.com>
2023-08-11 18:18:38 -0600

    Update FletcherChecksum.java

--- fletcher_checksum/FletcherChecksum.java
+++ fletcher_checksum/FletcherChecksum.java
@@ -1,19 +1,32 @@
 /*************************************************************
  * <h1>Universidad del Valle de Guatemala</h1>
- * <h2>Redes - Sección 10</h2>
+ * <h2>Redes - Seccion 10</h2>
  * <h3>Laboratorio 2</h3>
  * 
  * Java (Fletcher Checksum)
  * 
  * @author: [
  *      Cristian Laynez,
- *      Mario de León
+ *      Mario de Leon
  * ]
  *************************************************************/
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Random;
+
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 
 class Emisor {
     private static final int binarySize = 8;
@@ -21,6 +34,7 @@ class Emisor {
     private String checksum;
     private ArrayList<byte[]> bloquesDe8Bits;
 
+    // Constructor de la clase Emisor
     public Emisor(String trama) {
         if (!esTramaBinariaValida(trama)) {
             throw new IllegalArgumentException("Error: La trama debe contener solo 0s y 1s.");
@@ -57,14 +71,16 @@ class Emisor {
         this.checksum = calcularFletcherChecksum(bloquesDe8Bits);
     }
 
+    // Obtener el checksum calculado
     public String getChecksum() {
         return checksum;
     }
 
+    // Obtener el mensaje final con bloques y checksum
     public String getMensajeFinal() {
         StringBuilder bloques_finales = new StringBuilder();
         for (byte[] bloque : bloquesDe8Bits) {
-            bloques_finales.append(byteToBinaryString(bloque)).append(" "); // Agregamos el espacio aquí
+            bloques_finales.append(byteToBinaryString(bloque)).append(" "); // Agregamos el espacio aqui
         }
         bloques_finales.append(checksum);
 
@@ -78,25 +94,35 @@ class Emisor {
         return final_mensaje.toString();
     }
 
+    // Funcion para simular ruido en el mensaje
+    public static String simularRuido(String mensaje, double probabilidad) {
+        StringBuilder mensajeConRuido = new StringBuilder();
+        Random random = new Random();
 
+        for (char c : mensaje.toCharArray()) {
+            if (random.nextDouble() < probabilidad) {
+                // Cambiar el bit si se cumple la probabilidad
+                mensajeConRuido.append((c == '0') ? '1' : '0');
+            } else {
+                mensajeConRuido.append(c);
+            }
+        }
 
+        return mensajeConRuido.toString();
+    }
+
+    // Calcular el checksum de Fletcher
     public static String calcularFletcherChecksum(ArrayList<byte[]> bloquesDe8Bits) {
         StringBuilder bloquesConcatenados = new StringBuilder();
         for (byte[] bloque : bloquesDe8Bits) {
             bloquesConcatenados.append(byteToBinaryString(bloque));
         }
-        System.out.println("Bloques Concatenados: " + bloquesConcatenados);
 
         // Eliminar espacios de la cadena concatenada
         String bloquesSinEspacios = bloquesConcatenados.toString().replaceAll(" ", "");
 
         // Dividir la cadena en grupos de 8 bits usando split()
         String[] binary_blocks = bloquesSinEspacios.split("(?<=\\G.{8})");
-
-        // Mostrar los grupos de 8 bits almacenados en el arreglo String[]
-        for (int i = 0; i < binary_blocks.length; i++) {
-            System.out.println("Bloque " + i + ": " + binary_blocks[i]);
-        }
 
         StringBuilder new_string = new StringBuilder("00000000"); // Initialize the sum to all zeros
 
@@ -109,7 +135,7 @@ class Emisor {
             	bit_sum = (bit_sum & 255) + 1;
             }
 
-         // Realiza la suma directamente en binario con longitud fija de 8 bits
+            // Realiza la suma directamente en binario con longitud fija de 8 bits
             String binary_total_sum = String.format("%8s", Integer.toBinaryString(bit_sum)).replace(' ', '0');
             new_string = new StringBuilder(binary_total_sum);
         }
@@ -123,7 +149,7 @@ class Emisor {
         return new_string.toString();
     }
 
-    // Function to convert byte to binary string
+    // Convertir un byte a una cadena binaria
     public static String byteToBinaryString(byte[] dataBytes) {
         StringBuilder binaryString = new StringBuilder();
         for (byte dataByte : dataBytes) {
@@ -134,6 +160,7 @@ class Emisor {
         return binaryString.toString();
     }
 
+    // Verificar si una trama es binaria valida
     public static boolean esTramaBinariaValida(String trama) {
         // Expresion regular para verificar que la trama contiene solo 0s y 1s
         String regex = "^[01]+$";
@@ -141,7 +168,8 @@ class Emisor {
         // Verificar si la trama coincide con la expresion regular
         return trama.matches(regex);
     }
-    
+
+    // Escribir el mensaje final en un archivo
     public void escribirMensajeFinalEnArchivo(String rutaArchivo) {
         try {
             // Abrir el archivo en modo escritura
@@ -168,21 +196,73 @@ class Emisor {
             System.out.println("Error al escribir el archivo: " + e.getMessage());
         }
     }
-
 }
+
 
 public class FletcherChecksum {
     public static void main(String[] args) {
-        String trama = "1010100100111001";
-        System.out.println("\n***EMISOR***");
-        Emisor emisor = new Emisor(trama);
+        // *************** Capa aplicacion ***************
+        int numMensajes = 10000; // Cantidad de mensajes a enviar
+        int longitudMensajeMin = 10; // Longitud mínima de los mensajes
+        int longitudMensajeMax = 100; // Longitud máxima de los mensajes
 
-        System.out.println("Checksum Fletcher: " + emisor.getChecksum());
-        System.out.println("Mensaje Final: " + emisor.getMensajeFinal());
-        
-        // Escribir el mensaje final en un archivo de texto
-        String rutaArchivo = "mensaje_emisor.txt";
-        emisor.escribirMensajeFinalEnArchivo(rutaArchivo);
+        for (int i = 0; i < numMensajes; i++) {
+            // Generar un mensaje aleatorio de longitud variable
+            int longitudMensaje = getRandomNumberInRange(longitudMensajeMin, longitudMensajeMax);
+            String mensaje = generateRandomMessage(longitudMensaje);
+
+            // *************** Capa presentacion ***************
+            StringBuilder tramaBinaria = new StringBuilder();
+            for (char c : mensaje.toCharArray()) {
+                String asciiBinary = String.format("%8s", Integer.toBinaryString(c)).replace(' ', '0');
+                tramaBinaria.append(asciiBinary);
+            }
+
+            System.out.println("\n***EMISOR***");
+            Emisor emisor = new Emisor(tramaBinaria.toString());
+
+            // *************** Capa enlace ***************
+            System.out.println("Checksum Fletcher: " + emisor.getChecksum());
+            System.out.println("Mensaje Final: " + emisor.getMensajeFinal());
+
+            // Escribir el mensaje final en un archivo de texto
+            String rutaArchivo = "mensaje_emisor.txt";
+            emisor.escribirMensajeFinalEnArchivo(rutaArchivo);
+
+            // *************** Capa ruido ***************
+            // Simular ruido agregando cambios aleatorios en algunos bits
+            String mensajeConRuido = Emisor.simularRuido(emisor.getMensajeFinal(), 0.01); // 10% de probabilidad de cambio
+
+            // *************** Capa transmision ***************
+            // Crear un socket del cliente para conectarse al receptor
+            String host = "localhost"; // Receptor
+            int port = 12345; // Puerto para la comunicacion
+
+            try (Socket socket = new Socket(host, port)) {
+                OutputStream output = socket.getOutputStream();
+                output.write(mensajeConRuido.getBytes());
+                System.out.println("Mensaje con ruido enviado al receptor.");
+            } catch (IOException e) {
+                System.out.println("Error al enviar el mensaje: " + e.getMessage());
+            }
+        }
+    }
+
+    // Genera un número aleatorio en un rango dado
+    private static int getRandomNumberInRange(int min, int max) {
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+
+    // Genera un mensaje aleatorio de longitud dada
+    private static String generateRandomMessage(int length) {
+        Random r = new Random();
+        StringBuilder message = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            char c = (char) (r.nextInt(26) + 'a');
+            message.append(c);
+        }
+        System.out.println(message.toString());
+        return message.toString();
     }
 }
-
